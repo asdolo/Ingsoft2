024e97c6cee48e108d1267764d5b78bfde6e109e
Lucas Raposeiras <lucas.raposeiras@outlook.com>
2020-05-14 19:30:51 -0300

    Análisis completo

--- taller-2/soot-dataflow-analysis/zero-analysis/src/main/java/dataflow/ZeroLatticeValueVisitor.java
+++ taller-2/soot-dataflow-analysis/zero-analysis/src/main/java/dataflow/ZeroLatticeValueVisitor.java
@@ -12,60 +12,61 @@ import dataflow.utils.ValueVisitor;
 import soot.Local;
 
 public class ZeroLatticeValueVisitor extends AbstractValueVisitor<ZeroLattice> {
-
-  private final Map<String, ZeroLattice> variables;
-  private ZeroLattice resolvedValue = ZeroLattice.BOTTOM;
-  private Boolean possibleDivisionByZero;
-
-  public ZeroLatticeValueVisitor(Map<String, ZeroLattice> variables) {
-    this.variables = variables;
-    this.possibleDivisionByZero = false;
-  }
-
-  @Override
-  public void visitLocal(Local variable) {
-    resolvedValue = variables.get(variable.getName());
-  }
-
-  @Override
-  public void visitDivExpression(ZeroLattice leftOperand, ZeroLattice rightOperand) {
-    // Tener en cuenta que this.possibleDivisionByZero indica que hay una posible division.
-    throw new UnsupportedOperationException();
-  }
-  
-
-  @Override
-  public void visitMulExpression(ZeroLattice leftOperand, ZeroLattice rightOperand) {
-    resolvedValue = leftOperand.multiplyBy(rightOperand);
-  }
-
-  @Override
-  public void visitSubExpression(ZeroLattice leftOperand, ZeroLattice rightOperand) {
-    resolvedValue = leftOperand.substract(rightOperand);
-  }
-
-  @Override
-  public void visitAddExpression(ZeroLattice leftOperand, ZeroLattice rightOperand) {
-    resolvedValue = leftOperand.add(rightOperand);
-  }
-
-  @Override
-  public void visitIntegerConstant(int value) {
-    // Tener en cuenta que this.resolvedValue contiene el valor abstracto que se quiere devolver.
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public ZeroLattice done() {
-    return resolvedValue;
-  }
-
-  @Override
-  public ValueVisitor cloneVisitor() {
-    return new ZeroLatticeValueVisitor(variables);
-  }
-
-  public Boolean getPossibleDivisionByZero() {
-    return possibleDivisionByZero;
-  }
+	
+	private final Map<String, ZeroLattice> variables;
+	private ZeroLattice resolvedValue = ZeroLattice.BOTTOM;
+	private Boolean possibleDivisionByZero;
+	
+	public ZeroLatticeValueVisitor(Map<String, ZeroLattice> variables) {
+		this.variables = variables;
+		this.possibleDivisionByZero = false;
+	}
+	
+	@Override
+	public void visitLocal(Local variable) {
+		resolvedValue = variables.get(variable.getName());
+	}
+	
+	@Override
+	public void visitDivExpression(ZeroLattice leftOperand, ZeroLattice rightOperand) {
+		resolvedValue = leftOperand.divideBy(rightOperand);
+		possibleDivisionByZero = rightOperand != ZeroLattice.NOT_ZERO;
+	}
+	
+	
+	@Override
+	public void visitMulExpression(ZeroLattice leftOperand, ZeroLattice rightOperand) {
+		resolvedValue = leftOperand.multiplyBy(rightOperand);
+	}
+	
+	@Override
+	public void visitSubExpression(ZeroLattice leftOperand, ZeroLattice rightOperand) {
+		resolvedValue = leftOperand.substract(rightOperand);
+	}
+	
+	@Override
+	public void visitAddExpression(ZeroLattice leftOperand, ZeroLattice rightOperand) {
+		resolvedValue = leftOperand.add(rightOperand);
+	}
+	
+	@Override
+	public void visitIntegerConstant(int value) {
+		resolvedValue = value == 0
+			? ZeroLattice.ZERO
+			: ZeroLattice.NOT_ZERO;
+	}
+	
+	@Override
+	public ZeroLattice done() {
+		return resolvedValue;
+	}
+	
+	@Override
+	public ValueVisitor cloneVisitor() {
+		return new ZeroLatticeValueVisitor(variables);
+	}
+	
+	public Boolean getPossibleDivisionByZero() {
+		return possibleDivisionByZero;
+	}
 }
--- taller-2/soot-dataflow-analysis/zero-analysis/src/main/java/dataflow/abs/ZeroLattice.java
+++ taller-2/soot-dataflow-analysis/zero-analysis/src/main/java/dataflow/abs/ZeroLattice.java
@@ -1,46 +1,129 @@
 package dataflow.abs;
 
 /**
- * Lattice used in the DivisionByZeroAnalysis.
- */
+* Lattice used in the DivisionByZeroAnalysis.
+*/
 public enum ZeroLattice {
+	
+	BOTTOM("bottom"), NOT_ZERO("not-zero"), ZERO("zero"), MAYBE_ZERO("maybe-zero");
+	
+	private String name;
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+	ZeroLattice(String name) {
+		this.name = name;
+	}
+	
+	private boolean isBottom() {
+		return this.equals(ZeroLattice.BOTTOM);
+	}
 
-  BOTTOM("bottom"), NOT_ZERO("not-zero"), ZERO("zero"), MAYBE_ZERO("maybe-zero");
+	private boolean isNotZero() {
+		return this.equals(ZeroLattice.NOT_ZERO);
+	}
 
-  private String name;
+	private boolean isZero() {
+		return this.equals(ZeroLattice.ZERO);
+	}
+	
+	public ZeroLattice add(ZeroLattice another) {
+		// Devolveremos:
+		// - ZERO si ambos sumandos son el valor abstracto ZERO
+		//    (ya que sabemos que 0 + 0 = 0)
+		// - NOT_ZERO si un sumando es el valor abstracto ZERO y el otro sumando es el valor abstracto NOT_ZERO
+		//    (ya que, al ser cero el elemento neutro de la suma/resta, al operar con un valor abstracto NOT_ZERO obtendremos el mismo valor abstracto)
+		// - MAYBE_ZERO en otro caso
 
-  @Override
-  public String toString() {
-    return this.name;
-  }
+		if (this.isZero() && another.isZero()) {
+			return ZeroLattice.ZERO;
+		}
 
-  ZeroLattice(String name) {
-    this.name = name;
-  }
+		if (this.isZero() && another.isNotZero() || this.isNotZero() && another.isZero()) {
+			return ZeroLattice.NOT_ZERO;
+		}
 
-  public ZeroLattice add(ZeroLattice another) {
+		return ZeroLattice.MAYBE_ZERO;
+	}
+	
+	public ZeroLattice divideBy(ZeroLattice another) {
+		// Devolveremos:
+		// - ZERO si el dividendo es el valor abstracto ZERO y el divisor es el valor abstracto NOT_ZERO
+		//    (ya que en este caso el dividendo actúa como valor absorbente y la división se evalúa a cero)
+		// - NOT_ZERO si tanto el dividendo como el divisor son el valor abstracto NOT_ZERO
+		// - MAYBE_ZERO en otro caso
+		//    (cabe destacar que el caso donde el divisor es ZERO cae en este caso. simplemente elijo devolver MAYBE_ZERO como convención)
 
-    throw new UnsupportedOperationException();
-  }
-  
+		if (this.isZero() && another.isNotZero()) {
+			return ZeroLattice.ZERO;
+		}
 
-  public ZeroLattice divideBy(ZeroLattice another) {
-    throw new UnsupportedOperationException();
-  }
-  
+		if (this.isNotZero() && another.isNotZero()) {
+			return ZeroLattice.NOT_ZERO;
+		}
 
-  public ZeroLattice multiplyBy(ZeroLattice another) {
-    throw new UnsupportedOperationException();
-  }
-  
-
-  public ZeroLattice substract(ZeroLattice another) {
-    throw new UnsupportedOperationException();
-  }
-  
-
-  public ZeroLattice supreme(ZeroLattice another) {
-    throw new UnsupportedOperationException();
-  }
-  
+		return ZeroLattice.MAYBE_ZERO;
+	}
+	
+	
+	public ZeroLattice multiplyBy(ZeroLattice another) {
+		// Devolveremos:
+		// - ZERO si ALGUNO de los dos valores abstractos es ZERO
+		//    (cero es el elemento absorbente de la multiplicación)
+		// - NOT ZERO si AMBOS valores abstractos son NOT_ZERO
+		//    (ya que sabemos que dos valores distintos de cero multiplicados dan un valor distinto de cero)
+		// - MAYBE_ZERO en otro caso
+		
+		if (this.isZero() || another.isZero()) {
+			return ZeroLattice.ZERO;
+		}
+		
+		if (this.isNotZero() && another.isNotZero()) {
+			return ZeroLattice.NOT_ZERO;
+		}
+		
+		return ZeroLattice.MAYBE_ZERO;
+	}
+	
+	public ZeroLattice substract(ZeroLattice another) {
+		// Devolveremos:
+		// - ZERO si tanto el el minuendo como el sustraendo son el valor abstracto ZERO
+		//    (ya que sabemos que 0 - 0 = 0)
+		// - NOT_ZERO si uno de los valores abstractos es ZERO y el otro es NOT_ZERO
+		//    (ya que, al ser cero el elemento neutro de la suma/resta, al operar con un valor abstracto NOT_ZERO obtendremos el mismo valor abstracto)
+		// - MAYBE_ZERO en otro caso
+		
+		if (this.isZero() && another.isZero()) {
+			return ZeroLattice.ZERO;
+		}
+		
+		if (this.isZero() && another.isNotZero() || this.isNotZero() && another.isZero()) {
+			return ZeroLattice.NOT_ZERO;
+		}
+		
+		return ZeroLattice.MAYBE_ZERO;
+	}
+	
+	public ZeroLattice supreme(ZeroLattice another) {
+		
+		// Seguro que el otro valor es mayor o,
+		// en el peor caso, es BOTTOM también
+		if (this.isBottom()) {
+			return another;
+		}
+		
+		if (another.isBottom()) {
+			return this;
+		}
+		
+		// Si son iguales, devuelvo cualquiera de ellos
+		if (this.equals(another)) {
+			return this;
+		}
+		
+		return ZeroLattice.MAYBE_ZERO;
+	}
 }
